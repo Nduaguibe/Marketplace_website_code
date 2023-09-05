6280a663f97f875dca55b8636608962e66261be1
Nduaguibe Kelechi <bigkelly126@gmail.com>
2023-09-05 19:44:37 +0100

    David ive pushed it

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,7 @@ def book_view(request,category):
     cat = get_object_or_404(Category,name=category) 
     books = item.objects.filter(category_id=cat.id)
     # books = get_object_or_404(item,category=category)
-    return render(request,'dashboard/book_view.html',{'books':books})
+    return render(request,'dashboard/book_view.html',{'books':books,'cat':cat})
 
 
 def phone_view(request):
